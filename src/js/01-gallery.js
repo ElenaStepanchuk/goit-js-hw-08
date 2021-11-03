bcbfa478e732a476b6a9a91532a15c5840d50378
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-03 19:30:01 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,36 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galeryElementsRefs = document.querySelector('.gallery');
+ 
+
+const createGalleryItems = galleryItems
+    .map(({ preview, original, description  }, index) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}" data-lightbox="roadtrip">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      data-index="${index}"
+      alt="${description}"/>
+  </a>
+</div>`;
+    })
+    .join('');   
+
+galeryElementsRefs.insertAdjacentHTML('beforeend', createGalleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    nav: true,   
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    close: true,
+});
+    
