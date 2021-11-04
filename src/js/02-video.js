9c933ed0785bb6db17e5b47327f7e8d6d7884b4d
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-04 17:15:19 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,10 @@
 import Player from '@vimeo/player';
 
-{/* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
 
-<script src="https://player.vimeo.com/api/player.js"></script>
-<script>
-    var iframe = document.querySelector('iframe');
-    var player = new Vimeo.Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-</script> */}
+player.on('play', function() {
+    console.log('played the video!');
+});
