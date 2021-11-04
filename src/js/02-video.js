e6c1ea5e42e2f9c031df3a770052ae67389e5bb1
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-04 21:09:06 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,13 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
+console.log(iframe);
+console.log(iframePlayer);
+
+
+const paused = iframePlayer.getPaused(iframe);
+console.log(paused)
+
