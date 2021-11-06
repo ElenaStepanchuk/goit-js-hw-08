d14d9df55ce627fa7f08eda56a441536a1841a9c
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-06 12:23:50 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,42 @@
-import Player from '@vimeo/player';
+import player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
 const iframePlayer = new Vimeo.Player(iframe);
+
+throttle = require('lodash.throttle');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const throttleTime = _.throttle(onGetTimePlayer, 1000);
+
 console.log(iframe);
 console.log(iframePlayer);
 
+player.on('play', function() {
+        console.log('played the video!');
+    });
 
-const paused = iframePlayer.getPaused(iframe);
-console.log(paused)
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
 
+
+function onGetTimePlayer() {
+    player
+        .getCurrentTime()
+        .then(function (seconds) {
+            // seconds = the current playback position
+            localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+        }).catch(function (error) {
+            // an error occurred
+            console.log(error);
+        });
+}
+
+player.on('timeupdate', throttleTime);
+
+const getCurrentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+
+player.setCurrentTime(getCurrentTime).catch(function(error) {
+    console.log(error);
+});
