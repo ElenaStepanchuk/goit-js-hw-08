237648c57f165cca9bc435562b0c80a13b0f831e
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-08 16:05:02 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,22 @@
 import player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
 const iframe = document.querySelector('iframe');
 const iframePlayer = new Vimeo.Player(iframe);
 const STORAGE_KEY = 'videoplayer-current-time';
-
-iframePlayer.on('timeupdate', throttle(throttleTime, 1000));
-
 
 function throttleTime(currentTime) {
     const videoTime = JSON.stringify(currentTime);
     localStorage.setItem(STORAGE_KEY, videoTime);
 }
-
-iframe.addEventListener('play', handleCurrentTime);
+iframePlayer.on('timeupdate', throttle(throttleTime, 1000));
 
 function handleCurrentTime() {
   const savedCurrentTime = JSON.stringify(currentTime);
   localStorage.setItem(STORAGE_KEY, savedCurrentTime);
 }
+
+iframe.addEventListener('play', handleCurrentTime);
 
 let savedTime = localStorage.getItem(STORAGE_KEY);
 let parsedTime = JSON.parse(savedTime);
