e70538e765a650ec0dc247d2298c4356dd4c9208
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-09 08:40:45 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    }
+refs.form.addEventListener('submit', handleFormSubmit);
+refs.email.addEventListener('input', handleEmailInput);
+refs.textarea.addEventListener('input', handleTextareaInput);
 
+function handleFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+function handleEmailInput(event) {
+    const email = event.currentTarget.value;
+    localStorage.setItem('feedback-form-state', email);
+    // console.log(message);
+}
+
+function handleTextareaInput(event) {
+    const message = event.currentTarget.value;
+    localStorage.setItem('feedback-form-state', message);
+    // console.log(message);
+}
+function populateTextarea() {
+    const savedMessage = localStorage.getItem('feedback-form-state');
+    if (savedMessage) {
+        refs.textarea.value = savedMessage;
+    }
+}
