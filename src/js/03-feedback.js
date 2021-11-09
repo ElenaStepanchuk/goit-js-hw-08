adb38fcbf5cd365c6ab6d8349f84d076718dae11
ElenaStepanchuk <podolyak365@gmail.com>
2021-11-09 13:34:23 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,40 +11,33 @@ const formData = {};
 
 
 refs.form.addEventListener('submit', handleFormSubmit);
-// refs.email.addEventListener('input', handleEmailInput);
-refs.textarea.addEventListener('input', throttle(handleTextareaInput, 500));
-
-refs.form.addEventListener('input', event => {
-    // console.log(event.target.name);
-    // console.log(event.target.value);
-
-    formData[event.target.name] = event.target.value;
-    console.log(formData);
-})
-
-populateTextarea();
+refs.form.addEventListener('input', throttle(handleSaveTextInput, 500));
 
 function handleFormSubmit(event) {
     event.preventDefault();
-    event.currentTarget.reset();
+    formData[event.target.name] = event.target.value;
+    console.log(formData);
+    handleDelText()
+}
+function handleDelText() {
+    refs.form.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
 
-// function handleEmailInput(event) {
-//     const email = event.currentTarget.value;
-//     localStorage.setItem(STORAGE_KEY, email);
-//     // console.log(message);
-// }
-
-function handleTextareaInput(event) {
-    const message = event.target.value;
-    localStorage.setItem(STORAGE_KEY, message);
+function handleSaveTextInput(event) {
+    formData[event.target.name]  = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
     // console.log(message);
 }
 function populateTextarea() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    let savedMessage = localStorage.getItem(STORAGE_KEY);
     if (savedMessage) {
+        savedMessage = JSON.parse(savedMessage);
         console.log(savedMessage);
-        refs.textarea.value = savedMessage;
+        Object.entries(savedMessage).forEach(([key, value]) => {
+        formData[key] = value
+        refs.form.elements[key].value = value  
+        })
     }
 }
+populateTextarea();
